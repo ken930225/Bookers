8f0d608541750cf9d43a7279dd3941e4dc1cacee
浅野謙介 <asanokensuke@asanokensukenoMacBook-Air.local>
2020-07-14 21:47:40 +0900

    first commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '8846fd2a75c7960ead70d98c23676d5c44a2b074aa64e7b903529eb2caddc8ed0873b8f21dd57c256833b539107429d1a0a9d37374b58d0afd7d7726a58edc95'
+Refile.secret_key = 'e9cc9a10e7565866e81af4fe6ca5f0d24fba37baf949d397a5ba02c9e62f2ed18bcd89a6af2cae56cbbcdbcf2fe53089b73bf6c7fe8c0eed3b429767a27bc339'
